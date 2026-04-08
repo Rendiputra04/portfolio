6b3c2bfc47b93959c1465f45155ec6a3b65b2070
Rendiputra04 <rendiputraa2004@gmail.com>
2026-04-08 15:27:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-AOS.init();
+const reveals = document.querySelectorAll(".section, .hero");
 
-// DARK MODE
-function toggleDarkMode() {
-    document.body.classList.toggle("dark");
-}
-
-// FETCH DATA API
-async function loadProjects() {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=3");
-    const data = await res.json();
-
-    const container = document.getElementById("project-list");
-
-    data.forEach(item => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-
-        card.innerHTML = `
-            <h3>${item.title}</h3>
-            <p>${item.body}</p>
-        `;
-
-        container.appendChild(card);
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
     });
-}
+  },
+  {
+    threshold: 0.15,
+  }
+);
 
-loadProjects();
-
-// FORM
-function kirimPesan(e){
-    e.preventDefault();
-    alert("Pesan berhasil dikirim!");
-}
+reveals.forEach((section) => {
+  section.classList.add("reveal");
+  observer.observe(section);
+});
